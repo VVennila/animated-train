e98f9576db81e1392dd7373e367172ec9d918238
Vennila <nilarajan@gmail.com>
2024-10-10 19:56:03 +0800

    Initial commit with Node.js app

--- index.js
+++ index.js
@@ -2,26 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // To parse JSON bodies
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    console.log('Received request for /');
     res.send('Hello, Animated Train!');
 });
 
 app.get('/animate', (req, res) => {
-    console.log('Received request for /animate');
     res.send('Animation started!');
 });
 
 app.get('/page/:id', (req, res) => {
-    console.log(`Received request for /page/${req.params.id}`);
     res.send(`Page ${req.params.id}`);
-});
-
-app.post('/train', (req, res) => {
-    console.log(`Received POST request for /train with data: ${JSON.stringify(req.body)}`);
-    res.send(`Train ${req.body.trainId} status is ${req.body.status}`);
 });
 
 app.post('/train', (req, res) => {
